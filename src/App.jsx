942539bff26ee8645b008e2fdb1cd1a98cf4908d
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-11 17:14:26 -0500

    practicing card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Card from "./Card"
+
 
 function App() {
 
     return (
         <>
         
-            <Header />
-            <Food/>
-            <Footer/>
+        <Card/>
+           
         </>
     )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+
+function Card() {
+
+    return(
+        <div className="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8ruwjf0T59uJxzbfrQct6hfVeigpDm6aMjysy3UFpgh4gH3QtQGqVvY&s" alt="profile picture" ></img>
+            <h2>El Mehdi Oukid</h2>
+            <p>Software Engineering</p>
+        </div>
+    )
+
+}
+
+export default Card;
