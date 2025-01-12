c8e60c5694dd2dec3ea376982a51da039f770641
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-11 19:13:37 -0500

    using basing CSS to make it look like a card

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,9 +3,9 @@ function Card() {
 
     return(
         <div className="card">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8ruwjf0T59uJxzbfrQct6hfVeigpDm6aMjysy3UFpgh4gH3QtQGqVvY&s" alt="profile picture" ></img>
-            <h2>El Mehdi Oukid</h2>
-            <p>Software Engineering</p>
+            <img className="card-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ8ruwjf0T59uJxzbfrQct6hfVeigpDm6aMjysy3UFpgh4gH3QtQGqVvY&s" alt="profile picture" ></img>
+            <h2 className="card-title">El Mehdi Oukid</h2>
+            <p className="card-text">Software Engineering</p>
         </div>
     )
 
