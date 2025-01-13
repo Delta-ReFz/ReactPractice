768aa48a288d627d401dbdabe9ca93d6260a7974
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-12 20:54:08 -0500

    learned how props worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Students from "./Students"
 
 
-
-
 function App() {
 
     return (
         <>
-           <Students name="Elmo" age={23} isStudent={false}/>
+           <Students name="Elmo" age={23} isStudent={true}/>
+           <Students name="Lilop" age={21} isStudent={false}/>
+           <Students name="Byllz" age={14} isStudent={true}/>
+           <Students/>
         </>
     )
 }
