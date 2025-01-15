f97dfdb55efde8d08e1277226b0394766778e5db
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-15 11:50:19 -0500

    alphabetical & numerical order

--- src/List.jsx
+++ src/List.jsx
@@ -2,9 +2,19 @@
 
 function List() {
 
-    const fruits = ["apple", "orange", "banana", "coconut"];
+    const fruits = [{id: 1, name:"apple", calories:95},
+                    {id: 2, name:"orange", calories: 45}, 
+                    {id: 3, name:"banana",calories:105}, 
+                    {id: 4, name:"coconut", calories:159}];
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)
+    
+    //fruits.sort((a,b) => a.name.localeCompare(b.name)); //ALPHABETICAL ORDER
+   
+    //fruits.sort((a,b) => a.calories - b.calories); //NUMERICAL ORDER
+
+    
+
+    const listItems = fruits.map(fruit => <li key = {fruit.id}>{fruit.name}: {fruit.calories}</li>)
 
     return(<ol>{listItems}</ol>);
 
